e0d1504f84e683295bc11c1dfa6931fddf9ede99
katsiev1997 <katsiev1997@gmail.com>
2024-01-28 16:12:25 +0300

    Верстка

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from './components/Header.vue'
+import Drawer from './components/Drawer.vue'
+import CardList from './components/CardList.vue'
+</script>
+
+<template>
+  <Drawer />
+  <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14">
+    <Header />
+    <CardList />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Card from './Card.vue'
+const onClickAdd = () => {
+    alert('Добавить')
+}
+</script>
+
+<template>
+  <div class="p-10">
+    <h2 class="text-3xl font-bold mb-10">Все кроссовки</h2>
+    <div class="grid grid-cols-4 gap-5">
+      <Card
+        title="Мужские кроссовки Nike Blazer Mid Suede"
+        image-url="/sneakers/sneakers-1.jpg"
+        :price="5000"
+        :isFavorite="true"
+        :onClickAdd="onClickAdd"
+      />
+    </div>
+  </div>
+</template>
